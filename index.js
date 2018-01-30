3645ccdb3cfbe3cf71eedd289a5a775770be5dda
Vladislav Korbut <v.korbut8@gmail.com>
2018-01-31 00:30:46 +0300

    Add simple logic for city generation

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+
+const letters = require('./letters');
+
+const app = express();
+
+const randPosInArray = length => Math.floor(Math.random() * length);
+
+app.get('/get-city/:letter', function (req, res) {
+  const letter = req.params.letter.toLowerCase();
+  const cities = letters[letter];
+  if (cities && cities.length) {
+    res.json({city: cities[randPosInArray(cities.length)]});
+  } else {
+    res.status(400).json({error: 'Wrong query!'});
+  }
+});
+
+app.listen(3000);
