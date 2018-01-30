0e08b20a138cbd0d522116ddea672eeac6d82b8e
Vladislav Korbut <v.korbut8@gmail.com>
2018-01-31 00:44:13 +0300

    Add heroku run file

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const app = express();
 
 const randPosInArray = length => Math.floor(Math.random() * length);
 
-app.get('/get-city/:letter', function (req, res) {
+app.get('/', (req, res) => {
+  res.json({
+    name: 'vkrbt',
+  })
+})
+
+app.get('/get-city/:letter', (req, res) => {
   const letter = req.params.letter.toLowerCase();
   const cities = letters[letter];
   if (cities && cities.length) {
@@ -18,4 +24,4 @@ app.get('/get-city/:letter', function (req, res) {
   }
 });
 
-app.listen(80);
+app.listen(3000);
