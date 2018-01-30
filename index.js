dc5a2d0de5db98462caf57e8b1a399a535d76313
Vladislav Korbut <v.korbut8@gmail.com>
2018-01-31 00:58:23 +0300

    Port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const os = require('os');
 
 const letters = require('./letters');
 
@@ -18,10 +19,14 @@ app.get('/get-city/:letter', (req, res) => {
   const letter = req.params.letter.toLowerCase();
   const cities = letters[letter];
   if (cities && cities.length) {
-    res.json({city: cities[randPosInArray(cities.length)]});
+    res.json({ city: cities[randPosInArray(cities.length)] });
   } else {
-    res.status(400).json({error: 'Wrong query!'});
+    res.status(400).json({ error: 'Wrong query!' });
   }
 });
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on ${port} port`)
+});
